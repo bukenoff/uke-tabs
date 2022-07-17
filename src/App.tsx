845ad90cab3dad47bcd8330d9e3c7081527cefd9
serik <bukenoff.s@gmail.com>
2022-07-17 10:19:00 +0700

    Wrap in theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React, { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import ThemeProvider from 'react-bootstrap/ThemeProvider';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { store, persistor } from './redux/store';
 import SongContainer from './containers/SongContainer';
@@ -10,18 +12,27 @@ import DashboardContainer from './containers/DashboardContainer';
 import { initSentry } from './services/sentry';
 import './i18n';
 import './index.css';
+import 'normalize.css';
+import { Navigation } from './components/Navigation';
 
 initSentry();
 
 const App: FC = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<DashboardContainer />} />
-          <Route path="/song/:id" element={<SongContainer />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider
+        breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+      >
+        <BrowserRouter>
+          <main>
+            <Navigation />
+            <Routes>
+              <Route path="/" element={<DashboardContainer />} />
+              <Route path="/song/:id" element={<SongContainer />} />
+            </Routes>
+          </main>
+        </BrowserRouter>
+      </ThemeProvider>
     </PersistGate>
   </Provider>
 );
