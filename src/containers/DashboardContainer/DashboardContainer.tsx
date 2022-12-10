1607a3ada22549b04738d65da6e835db435a3695
serik <bukenoff.s@gmail.com>
2022-12-10 10:15:27 +0700

    Configure deploy to surge

--- src/containers/DashboardContainer/DashboardContainer.tsx
+++ src/containers/DashboardContainer/DashboardContainer.tsx
@@ -2,5 +2,5 @@ import React, { FC } from 'react';
 import Container from 'react-bootstrap/Container';
 
 export const DashboardContainer: FC = () => {
-  return <Container>lll </Container>;
+  return <Container>hello world</Container>;
 };
